263752923c6f1483a10e746816d0abef0f18fe23
shivam sahu <shivamsahu7770@gmail.com>
2023-05-20 17:24:16 +0530

    error msg set

--- requests/registerValidation.js
+++ requests/registerValidation.js
@@ -13,7 +13,11 @@ const handleregisterUserValidationErrors = (req, res, next) => {
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
       // If there are validation errors, send an error response
-      return res.status(400).json({ errors: errors.array() });
+        return res.status(400).json({ 
+            status:false,
+            error:req.__('VALIDATION_FAILED'),
+            errors: errors.array() 
+        });
     }
     // If validation succeeds, proceed to the next middleware or route handler
     next();
