4fc3eedaded28fdcc5ab99b83e842e1b4603d2c9
shivam sahu <shivamsahu7770@gmail.com>
2023-08-01 00:51:55 +0530

    unique key remove

--- controllers/admin/productController.js
+++ controllers/admin/productController.js
@@ -1,7 +1,12 @@
-addProduct =  (req,res)=>{
-    
+const { Product } = require('@models');
+
+addProduct = async (req,res)=>{
+    const {subCategoryId} = req.body
     // console.log(req.body)
-    return res.send({"data": req.body});
+    const newProduct = await Product.create({
+        subCategoryId : subCategoryId
+    })
+    return res.send({"data": newProduct});
 }
 
 module.exports = {
--- migrations/20230604131417-create-product.js
+++ migrations/20230604131417-create-product.js
@@ -10,7 +10,6 @@ module.exports = {
         type: Sequelize.INTEGER
       },
       subCategoryId:{
-        unique: true,
         allowNull: false,
         type: Sequelize.INTEGER
       },
